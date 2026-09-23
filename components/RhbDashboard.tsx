b38b55f421bd6c74ade0a47105cf5cc31cbd2318
AnonymousCrux <luca.joos@protonmail.ch>
2026-09-23 20:27:55 +0200

    Fix Supabase auth lock error in RhbDashboard

--- components/RhbDashboard.tsx
+++ components/RhbDashboard.tsx
@@ -11,7 +11,13 @@ import { useLanguage } from "./LanguageContext";
 
 const getSupabase = () => createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  {
+    auth: {
+      persistSession: false,
+      autoRefreshToken: false,
+    },
+  }
 );
 
 interface RhbRow {
